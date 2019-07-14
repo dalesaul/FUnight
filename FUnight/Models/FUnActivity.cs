6f03743dcb1a3ac73d77862776bebd7cbef0354b
Dale <dalesauljr@gmail.com>
2019-07-14 10:14:05 -0400

    adjusted CSS components

--- FUnight/Models/FUnActivity.cs
+++ FUnight/Models/FUnActivity.cs
@@ -12,23 +12,34 @@ namespace FUnight.Models
         public int Id { get; set; }
 
         [Required]
+        [Display(Name = "Suggested Date")]
         public DateTime SuggestedDate { get; set; }
 
         [Required]
+        [Display(Name = "Is Food Available?")]
         public bool FoodAvailable { get; set; }
 
         [Required]
+        [Display(Name = "Cost Estimate")]
         public double CostEstimate { get; set; }
 
         public int? Rating { get; set; }
 
         public string ApplicationUserId { get; set; }
+
+        [Display(Name = "Activity Type")]
         public int ActivityTypeId { get; set; }
+
+        [Display(Name = "User Group")]
         public int UserGroupId { get; set; }
 
+        [Display(Name = "Additional Comments")]
         public string ActyTypeComment { get; set; }
 
+        [Display(Name = "Activity Type")]
         public ActivityType ActivityType { get; set; }
+
+        [Display(Name = "User Group")]
         public UserGroup UserGroup { get; set; }
 
 
